ac2f25f48241b111d552f9b43fdaee53877e109a
root <root@localhost.localdomain>
2020-01-25 23:18:24 +0100

    editing setup

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 """Initialize config.json"""
 import json
+import sys
 import os
+import subprocess
 
-def validate(path1, path2):
-    return True if os.path.exists(path1) and os.path.exists(path2) else False
+def packages(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-def setup(confile):
+def setupjson(confile):
     with open(confile, 'r+') as conf:
         data = json.load(conf)
 
         data['complete'] = input("[*] complete torrents dir: ")
-        data['incomplete'] = input("[*] incomplete torrents dir: ")
         data['ip'] = input("[*] server's ip: ")
         data['port'] = int(input("[*] server's port: "))
         data['ready'] = True
 
-        if validate(data['complete'], data['incomplete']):
+        if os.path.exists(data["complete"]):
             conf.seek(0)
             conf.write(json.dumps(data, indent=4))
             conf.truncate()
@@ -23,15 +24,18 @@ def setup(confile):
             print("Invalid paths")
 
 if __name__ == "__main__":
+    owo = input("Would you like to receive desktop notifications on file transfers? (windows only) (y\\n) ")
+    if owo == 'y':
+        packages("win10toast")
     confile = "config.json"
     with open(confile, 'r+') as conf:
         data = json.load(conf)
         if data['ready'] is False:
-            setup(confile)
+            setupjson(confile)
         else:
             message = confile+" is already initialized, do you want to re-initialize it? (y\\n)"
             choice = input(message)
             if choice.lower() == 'y':
-                setup(confile)
+                setupjson(confile)
             else:
                 exit()
